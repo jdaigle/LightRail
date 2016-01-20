5fbdbbb000b9c632579a363cf734f7810e2fb08d
Joseph Daigle <joseph.daigle@gmail.com>
2016-01-20 11:07:22 -0500

    adding extension samples project

---
+++ Samples/Samples.Extensions/Logging/Sample.cs
@@ -0,0 +1,54 @@
+﻿using LightRail.Client;
+using LightRail.Client.InMemoryQueue;
+using LightRail.Client.log4net;
+using LightRail.Client.Logging;
+using LightRail.Client.NLog;
+
+namespace Samples.Extensions.Logging
+{
+    public class Sample
+    {
+        public void Execute()
+        {
+            // Logging is statically assigned at startup and should not be
+            // changed. So uncomment the logger you want to use.
+
+            //UseLog4Net();
+            //UseNLog();
+
+            // Create an InMemory ServiceBus
+            var bus = ServiceBus.Factory.CreateFromInMemory(cfg =>
+            {
+                // Listen for Messages on the named Queue
+                cfg.ReceiveFrom("DemoQueue", r =>
+                {
+                    // Register all handlers in the current assembly
+                    r.ScanForHandlersFromCurrentAssembly();
+                });
+            });
+
+            // Start the ServiceBus (starts any receivers)
+            bus.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                bus.Send(new Message1(), "DemoQueue");
+            }
+
+            // Stop the Service (stops any receivers) and blocks waiting for current messages to finish processing.
+            bus.Stop();
+        }
+
+        private static void UseNLog()
+        {
+            // Override the default LogManager to use Log4Net
+            LogManager.UseFactory<NLogLoggerFactory>();
+        }
+
+        private static void UseLog4Net()
+        {
+            // Override the default LogManager to use Log4Net
+            LogManager.UseFactory<Log4NetLoggerFactory>();
+        }
+    }
+}
---
+++ Samples/Samples.Extensions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Samples.Extensions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Logging.Sample().Execute();
+
+            var prev = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Press Any Key To Quite");
+            Console.ForegroundColor = prev;
+            Console.ReadKey();
+        }
+    }
+}
